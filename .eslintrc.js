5a0411073c9558edc23e825048cc57d47dc49f50
pando <spreadpando@gmail.com>
2021-05-24 13:07:56 -0600

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/eslint-recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        'react/react-in-jsx-scope': 'off',
+        'react/display-name': 'off',
+        'no-var': 2,
+        'no-undef': 2,
+        'no-param-reassign': 2,
+      }
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PageIndex = () => {
+  return (
+    <>
+    hello
+    </>
+  )
+}
+
+export default PageIndex
